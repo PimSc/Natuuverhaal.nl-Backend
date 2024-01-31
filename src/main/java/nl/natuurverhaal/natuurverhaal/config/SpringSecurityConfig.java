9216137afafed42cb8b18e9ac39319c4060eaa77
PimSc <pimschuitema@gmail.com>
2024-01-31 14:35:44 +0100

    save

--- src/main/java/nl/natuurverhaal/natuurverhaal/config/SpringSecurityConfig.java
+++ src/main/java/nl/natuurverhaal/natuurverhaal/config/SpringSecurityConfig.java
@@ -68,7 +68,7 @@ public class SpringSecurityConfig {
                         auth
                 // Wanneer je deze uncomments, staat je hele security open. Je hebt dan alleen nog een jwt nodig.
 //                .requestMatchers("/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/users").permitAll()
                 .requestMatchers(HttpMethod.GET,"/users").hasRole("ADMIN")
                 .requestMatchers(HttpMethod.POST,"/users/**").hasRole("ADMIN")
                 .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
