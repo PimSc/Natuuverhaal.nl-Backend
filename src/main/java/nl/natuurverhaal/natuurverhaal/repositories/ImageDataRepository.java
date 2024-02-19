a1259aa76352223c76c4657354a2bf4838276fff
PimSc <pimschuitema@gmail.com>
2024-02-19 13:25:10 +0100

    blogpost met afbeelding toevoegen

--- src/main/java/nl/natuurverhaal/natuurverhaal/repositories/ImageDataRepository.java
+++ src/main/java/nl/natuurverhaal/natuurverhaal/repositories/ImageDataRepository.java
@@ -5,3 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageDataRepository extends JpaRepository<ImageData, Long> {
 }
+
+
+
+
+
+
+
+
+//package nl.natuurverhaal.natuurverhaal.repositories;
+//
+//import nl.natuurverhaal.natuurverhaal.models.ImageData;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ImageDataRepository extends JpaRepository<ImageData, Long> {
+//}
